9a2e858698300c67e0fc784f45c590ea832ffefe
PhilWatson100 <Phil100Watson100@gmail.com>
2024-09-28 22:12:28 +0100

    question b

--- Lab1Answers.js
+++ Lab1Answers.js
@@ -1,7 +1,14 @@
 //Question 2 (a)
 
 //creating the arrow function
-const moduleName = () => "Data Representation and Querying";
+//const moduleName = (name) => "Data Representation and Querying";
 
 // Call the function and output the result to the console
-console.log(moduleName());
+//console.log(moduleName());
+
+
+//(b)
+const moduleName = (name) => name;
+
+// Call the function and output the result to the console
+console.log(moduleName("Data Representation and Querying"));
